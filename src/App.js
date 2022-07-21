6a619503ef4baaf94b39431fa440e069fbed57bb
odadki <93558662+odadki@users.noreply.github.com>
2022-07-21 13:35:26 -0700

    added all pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import CardList from "./CardList";
+import SearchBox from "./SearchBox";
+import ErrorBoundry from "./ErrorBoundry";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     robots: [],
+  //     searchfield: "",
+  //   };
+  // }
+
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState("");
+
+  // componentDidMount() {
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then((response) => response.json())
+  //     .then((users) => this.setState({ robots: users }));
+  // }
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        setRobots(users);
+      });
+  }, []);
+
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value);
+  };
+
+  const filteredRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  });
+
+  if (robots.length === 0) {
+    return <h1>Loading</h1>;
+  } else {
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={onSearchChange} />
+        <br />
+        <ErrorBoundry>
+          <CardList robots={filteredRobots} />
+        </ErrorBoundry>
+      </div>
+    );
+  }
 }
 
 export default App;
